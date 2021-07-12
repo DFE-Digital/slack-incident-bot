9aaa76f7d77d38684615e3b72b7a7bbc6fc8764d
James Glenn <47917431+JR-G@users.noreply.github.com>
2021-07-12 19:44:57 +0100

    Rescue name_taken error

--- bot/actions/slack_incident_listener.rb
+++ bot/actions/slack_incident_listener.rb
@@ -2,11 +2,17 @@ SlackRubyBotServer::Events.configure do |config|
   config.on :action, 'view_submission' do |action|
     action.logger.info "Received #{action[:payload][:type]}."
 
-    if action[:payload][:view][:app_id] == ENV['SLACK_APP_ID']
-      t1 = Time.now
-      SlackIncidentActions.new.open_incident(action)
-      t2 = Time.now
-      puts t2 - t1
+    begin
+      if action[:payload][:view][:app_id] == ENV['SLACK_APP_ID']
+        t1 = Time.now
+        SlackIncidentActions.new.open_incident(action)
+        t2 = Time.now
+        puts t2 - t1
+      end
+    rescue Slack::Web::Api::Errors::NameTaken => e
+      slack_client = Slack::Web::Client.new(token: ENV['SLACK_TOKEN'])
+      slack_client.chat_postEphemeral(channel: action[:payload][:user][:id], user: action[:payload][:user][:id],
+                                      text: 'That incident channel name has already been taken. Please try another.')
     end
     nil
   end
