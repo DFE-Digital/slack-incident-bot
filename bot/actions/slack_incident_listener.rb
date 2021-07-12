1a728a3dd6e6be6988e96b20e6923a4012830804
James Glenn <47917431+JR-G@users.noreply.github.com>
2021-07-12 18:21:56 +0100

    Add tests for the SlackIncidentActions class

--- bot/actions/slack_incident_listener.rb
+++ bot/actions/slack_incident_listener.rb
@@ -2,7 +2,7 @@ SlackRubyBotServer::Events.configure do |config|
   config.on :action, 'view_submission' do |action|
     action.logger.info "Received #{action[:payload][:type]}."
 
-    if action[:payload][:view][:app_id] == 'A021D8M1RT9'
+    if action[:payload][:view][:app_id] == ENV['SLACK_APP_ID']
       t1 = Time.now
       SlackIncidentActions.new.open_incident(action)
       t2 = Time.now
