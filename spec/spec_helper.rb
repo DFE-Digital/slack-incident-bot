5ea9419bc338b9fe97f7aa4d969732a7befa4e5f
Duncan Brown <duncanjbrown@gmail.com>
2021-10-21 19:48:31 +0100

    Remove custom database config
    Rely on Rails conventions for the database connection

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,11 +21,6 @@ VCR.configure do |config|
   end
 end
 
-db_config = YAML.safe_load(File.read(File.expand_path('../config/postgresql.yml', __dir__)), [], [],
-                           true)[ENV['RACK_ENV']]
-ActiveRecord::Tasks::DatabaseTasks.create(db_config)
-ActiveRecord::Base.establish_connection(db_config)
-
 RSpec.configure do |config|
   config.before :suite do
     DatabaseCleaner.strategy = :deletion
