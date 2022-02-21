c0e40469557919ea8d9c7b5cd4ddebe34e2b4248
Duncan Brown <duncanjbrown@gmail.com>
2022-02-21 15:16:18 +0000

    Start moving to full-stack request specs
    - Create an Incident model to wrap the caching behaviour and provide a
      seam to stub tests against
    - Remove mysterious and complicated VCR
    - Create small set of stubs for Slack calls we expect to make
    - Use the whole stack in the tests, not just the gem-provided middleware

--- spec/requests/close_spec.rb
+++ spec/requests/close_spec.rb
@@ -1,59 +1,71 @@
 require 'rails_helper'
 
 describe 'slash_commands/close' do
-  include Rack::Test::Methods
-
-  def app
-    SlackRubyBotServer::Api::Middleware.instance
+  before do
+    disable_slack_signature_checks!
+    incident_started_in('twd_git_bat')
   end
 
-  context 'without signature checks' do
-    before do
-      allow_any_instance_of(Slack::Events::Request).to receive(:verify!)
+  let(:payload) do
+    {
+      command: '/closeincident',
+      text: '`',
+      channel_id: channel_id,
+      channel_name: channel_id,
+      user_id: 'user_id',
+      team_id: 'team_id',
+      token: 'deprecated',
+    }
+  end
+
+  context 'with a valid incident channel' do
+    let(:channel_id) { 'incident_channel_id' }
+    let(:channel_name) { 'incident_channel_name' }
+
+    it 'returns incident closed confirmation' do
+      stub_slack_message(channel: 'incident_channel_id', message: '<!here> This incident has now closed.')
+      stub_slack_pin(channel: 'incident_channel_id')
+      stub_slack_message(channel: 'twd_git_bat', message: ':white_check_mark: <!channel> The incident in <#incident_channel_id> has now closed.')
+
+      post '/api/slack/command', params: payload
+
+      expect(JSON.parse(response.body)).to eq('text' => 'You’ve closed the incident.')
+      expect(response.status).to eq 201
     end
+  end
 
-    context 'with a valid incident channel' do
-      let!(:web_client) { SlackMock.web_client }
+  context 'with an invalid incident channel' do
+    let(:channel_id) { 'some_other_channel_id' }
+    let(:channel_name) { 'some_other_channel_name' }
 
-      let(:command) do
-        {
-          command: '/closeincident',
-          text: '`',
-          channel_id: 'channel',
-          channel_name: 'incident_channel_name',
-          user_id: 'user_id',
-          team_id: 'team_id',
-          token: 'deprecated',
-        }
-      end
-
-      it 'returns incident closed confirmation' do
-        post '/api/slack/command', command
-        expect(last_response.status).to eq 201
-        expect(JSON.parse(last_response.body)).to eq('text' => 'You’ve closed the incident.')
-      end
+    it 'returns an error message' do
+      post '/api/slack/command', params: payload
+      expect(response.status).to eq 201
+      expect(JSON.parse(response.body)).to eq('text' => 'This is not an incident channel.')
     end
+  end
 
-    context 'with an invalid incident channel' do
-      let!(:web_client) { SlackMock.web_client }
+  def incident_started_in(calling_channel)
+    allow_any_instance_of(Incident).to receive(:calling_channel).and_return(calling_channel)
+  end
 
-      let(:command) do
-        {
-          command: '/closeincident',
-          text: '`',
-          channel_id: 'channel',
-          channel_name: 'channel_name',
-          user_id: 'user_id',
-          team_id: 'team_id',
-          token: 'deprecated',
-        }
-      end
+  def stub_slack_message(channel:, message:)
+    stub_request(:post, 'https://slack.com/api/chat.postMessage')
+      .with(body: { 'channel' => channel, 'text' => message })
+      .to_return(status: 200, body: dummy_slack_response)
+  end
 
-      it 'returns incident closed confirmation' do
-        post '/api/slack/command', command
-        expect(last_response.status).to eq 201
-        expect(JSON.parse(last_response.body)).to eq('text' => 'This is not an incident channel.')
-      end
-    end
+  def stub_slack_pin(channel:)
+    stub_request(:post, 'https://slack.com/api/pins.add')
+      .with(body: { 'channel' => channel })
+      .to_return(status: 200, body: dummy_slack_response)
+  end
+
+  def dummy_slack_response
+    { ok: true, channel: 'does not matter' }.to_json
+  end
+
+  def disable_slack_signature_checks!
+    allow_any_instance_of(Slack::Events::Request).to receive(:verify!)
   end
 end
