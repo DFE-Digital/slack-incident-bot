0868badd17ab47dd9db78ec059e72c2505ef31ea
James Glenn <47917431+JR-G@users.noreply.github.com>
2022-04-22 18:20:08 +0100

    Minor rubocop linting

--- bot/slash_commands/ping.rb
+++ bot/slash_commands/ping.rb
@@ -1,5 +1,5 @@
 SlackRubyBotServer::Events.configure do |config|
-  config.on :command, '/ping' do |command|
+  config.on :command, '/ping' do |_command|
     Rails.logger.info 'Received a ping, responding with pong.'
     { text: 'pong' }
   end
