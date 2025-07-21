fe66bf5bbef819816bf53c83495ce0fb50ab9b9e
Carlos Martinez <carlosjmtz@gmail.com>
2025-07-21 20:11:46 +0100

    ENV_URL added to hosts in the wrong place

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,5 @@ module SlackRailsBotStarter
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    config.hosts << ENV.fetch('ENV_URL')
   end
 end
